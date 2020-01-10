4646796ce00f7099b3f92891bd926d6909d37dea
Roman093 <58482842+Roman093@users.noreply.github.com>
2020-01-10 21:47:07 +0200

    Add files via upload

---
+++ customList2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Node
+    {
+        public Node(Notebook element, Node nextElement = null)
+        {
+            Element = element;
+            NextElement = nextElement;
+        }
+        public Notebook Element { get; set; }
+        public Node NextElement { get; set; }
+    }
+    public class CustomList : IEnumerable, ICustomLink
+    {
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        public Notebook this[int index] => throw new NotImplementedException();
+
+        public void Add(Notebook value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Notebook value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Notebook
+    {
+        public int SerialNumber { get; set; }
+        public string Name { get; set; }
+    }
+    public interface ICustomLink
+    {
+        void Add(Notebook value);
+        Notebook this[int index] { get; }
+        void Delete(Notebook value);
+    }
+}
